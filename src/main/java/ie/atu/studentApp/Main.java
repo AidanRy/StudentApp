ff8f1d0f9ab43dc5106aaf0b3c1a814612b8fbf6
Aidan <g00365056@atu.ie>
2022-10-23 20:40:25 +0100

    created StudentApp and StudentClass

---
+++ src/main/java/ie/atu/studentApp/Main.java
@@ -0,0 +1,9 @@
+package ie.atu.studentApp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter Student Name");
+
+    }
+}
---
+++ src/main/java/ie/atu/studentApp/StudentClass.java
@@ -0,0 +1,4 @@
+package ie.atu.studentApp;
+
+public class StudentClass {
+}
