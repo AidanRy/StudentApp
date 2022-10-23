05f18b928e261b929da938d5b921a72a41c8777a
Aidan <g00365056@atu.ie>
2022-10-23 21:02:00 +0100

    added getters and setters

--- src/main/java/ie/atu/studentApp/Main.java
+++ src/main/java/ie/atu/studentApp/Main.java
@@ -1,9 +1,12 @@
 package ie.atu.studentApp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Please enter Student Name");
+        Scanner input = new Scanner(System.in);
 
     }
 }
