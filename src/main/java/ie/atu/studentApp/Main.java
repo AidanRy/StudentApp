cc375d0154b38f9fcb76e9d86d70d670e28d5122
Aidan <g00365056@atu.ie>
2022-10-24 21:19:16 +0100

    added first instance working

--- src/main/java/ie/atu/studentApp/Main.java
+++ src/main/java/ie/atu/studentApp/Main.java
@@ -7,6 +7,19 @@ public class Main {
 
         System.out.println("Please enter Student Name");
         Scanner input = new Scanner(System.in);
+        Student student1 = new Student();
+        String name = input.nextLine();
+        student1.setName(name);
+        System.out.println("Please enter Student email");
+        String email = input.nextLine();
+        student1.setEmail(email);
+        System.out.println("Please enter Student course");
+        String course= input.nextLine();
+        student1.setCourse(course);
+        System.out.println("You entered : " + student1.getName());
+        System.out.println("student email :"+ student1.getEmail());
+        System.out.println("student course :"+ student1.getCourse());
+
 
     }
 }
